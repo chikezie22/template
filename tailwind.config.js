5ce21bd00385efa0ca777698e00ec8eab14183c4
chikezie22 <simonchikezie44@gmail.com>
2024-12-10 13:45:44 +0100

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        mulish: ["Mulish", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
