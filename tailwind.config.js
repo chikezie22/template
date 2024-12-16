064054b640051c8da9bf0d2ccede8ab57c9f7b6b
chikezie22 <simonchikezie44@gmail.com>
2024-12-16 15:16:21 +0100

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,10 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"],
         mulish: ["Mulish", "sans-serif"],
+        bebas: ["Bebas Neue", "sans-serif"],
       },
     },
   },
